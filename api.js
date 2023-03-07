27561e8a186f329892318c3da997ee6ff5cd5c5c
jaloliddimirzayev <jaloliddintuitdif3152gmail.com>
2023-03-07 15:50:49 +0500

    first commit

--- api.js
+++ api.js
@@ -1,7 +1,4 @@
-function searchFunc() {
-  const searchInput = document.getElementById("qiymat").value;
-  const apiUrl = `https://api.pexels.com/v1/search?query=${searchInput}&per_page=10&page=1`;
 
   fetch(`https://uz.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=TATU`)
     .then((Response) => Response.json())
-    .then((data) => console.log(data))
+    .then(data => console.log(data))
