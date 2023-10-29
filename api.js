f758ce5269582a851f375ed063daf5aa9da8a634
Jaloliddin <jaloliddintuitdif315@gmail.com>
2023-10-29 14:46:18 +0500

    wiki img

--- api.js
+++ api.js
@@ -1,4 +1,4 @@
 
-  fetch(`https://uz.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=TATU`)
+  fetch(`https://uz.wikipedia.org/w/api.php`)
     .then((Response) => Response.json())
     .then(data => console.log(data))
--- main.js
+++ main.js
@@ -1,55 +1,52 @@
-  const searchForm = document.getElementById("form");
-  const searchInput = document.getElementById("qiymat");
-  const searchResult = document.querySelector(".wiki");
+const searchForm = document.getElementById("form");
+const searchInput = document.getElementById("qiymat");
+const searchResult = document.querySelector(".wiki");
 const select = document.getElementById("select");
-  // const selectSon = document.getElementById("selectSon");
-  // const selectHajm = document.getElementById("selectHajm");
-  const apiUrl = "https://";
-  const apirUrl2 = ".wikipedia.org/w/api.php"
-  const searchParams = {
-    action: "query",
-    list: "search",
-    format: "json",
-    prop: "extracts",
-    exsentences: "2",
-    exlimit: "500",
-    origin: "*",
-  };
-  // const google = "https://picsum.photos/200/";
-  searchInput.addEventListener("input", (event) => {
-    event.preventDefault();
-    const searchTerm = searchInput.value.trim();
-    if (searchTerm === "") {
-      return;
-    }
-    searchParams.srsearch = searchTerm;
-    const queryString = new URLSearchParams(searchParams).toString();
-    fetch(`${apiUrl + select.value + apirUrl2}?${queryString}`)
-      .then((response) => response.json())
-      .then((data) => {
-        const searchResultHtml = data.query.search
-          .map(
-            (result) => `
-              <div class="coctile-item">
-              <div class="id-name-time">
+const apiUrl = "https://";
+const apirUrl2 = ".wikipedia.org/w/api.php";
+const searchParams = {
+  action: "query",
+  list: "search",
+  format: "json",
+  prop: "extracts|pageimages",
+  exsentences: "2",
+  exlimit: "500",
+  piprop: "original",
+  origin: "*",
+};
+const imageBaseUrl = "https://commons.wikimedia.org/wiki/Special:FilePath/";
+
+searchInput.addEventListener("input", (event) => {
+  event.preventDefault();
+  const searchTerm = searchInput.value.trim();
+  if (searchTerm === "") {
+    return;
+  }
+  searchParams.srsearch = searchTerm;
+  const queryString = new URLSearchParams(searchParams).toString();
+  fetch(`${apiUrl + select.value + apirUrl2}?${queryString}`)
+    .then((response) => response.json())
+    .then((data) => {
+      const searchResultHtml = data.query.search
+        .map((result) => `
+          <div class="coctile-item">
+            <div class="id-name-time">
               <p>ID: ${result.pageid} </p>
-                <p>NAME: ${result.title} haqida </p>
-          </div>
-          <div class="images">
-                    <img src="" alt="">
-          </div>
-          <div class="item-text">
+              <p>NAME: ${result.title} haqida </p>
+            </div>
+            <div class="images">
+              <img src="${imageBaseUrl}${result.title}.jpg" alt="">
+            </div>
+            <div class="item-text">
               <p>${result.snippet}</p>
+            </div>
           </div>
-          </div>
-          `
-          )
-          .join("");
-        searchResult.innerHTML = searchResultHtml;
-      })
-      .catch((error) => {
-        console.error(error);
-        searchResult.innerHTML = "<p>Natija topilmadi</p>";
-      });
-  });
-  
+        `)
+        .join("");
+      searchResult.innerHTML = searchResultHtml;
+    })
+    .catch((error) => {
+      console.error(error);
+      searchResult.innerHTML = "<p>Natija topilmadi</p>";
+    });
+});
